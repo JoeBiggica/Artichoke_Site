1372e120e74507eeb73ae964afca464aa91969a1
JoeBiggia <jbaki91@yahoo.com>
2017-11-14 22:02:54 -0500

    added buttons and new modules

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
+import TextBanner from '../../modules/text-banner';
+
 import styles from './Home.scss';
 
 
 export default class Home extends Component {
 
 	render() {
+		const text_banner_props = {
+			background: 'green',
+			text: 'Come see what we have to offer and let us reveal the heart of your business.',
+			centered: true,
+			button: {
+				path: '/contact-us',
+				text: 'Let\'s work together'
+			}
+		}
+
 		return(
 			<div className={styles('container')}>
 				<div className={styles('banner')}>
-					<span className={styles('banner-text')}> We Handle the Johns, </span>
-					<span className={styles('banner-text')}> You Enjoy the Boyegas, </span>
+					<div className={styles('inner')}>
+						<span className={styles('banner-text')}>We Handle the Johns,</span>
+						<span className={styles('banner-text')}>You Enjoy the Boyegas</span>
+					</div>
 				</div>
 				<div className={styles('cards-container')}>
 					<div className={styles('card')}>
@@ -37,12 +51,13 @@ export default class Home extends Component {
 					<div className={styles('inner')}>
 						<div className={styles('thumb')} />
 						<div className={styles('info')}>
-							<h3 className={styles('title')}>Areas of Expertise</h3>
-							<p>The Heart of Design - Literal or metaphorical, it\'s our responsibility at Artichoke Designs to handle the layers. We are committed to making the experience for our clients as simple as possible, while providing excellent communication and updates throughout the process. Our experience and dedication to our work allows us to treat any project, no matter how big or small, with the utmost care that our customers demand and deserve.</p>
+							<h3 className={styles('title')}>The Heart of Design</h3>
+							<p>Literal or metaphorical, it\'s our responsibility at Artichoke Designs to handle the layers. We are committed to making the experience for our clients as simple as possible, while providing excellent communication and updates throughout the process. Our experience and dedication to our work allows us to treat any project, no matter how big or small, with the utmost care that our customers demand and deserve.</p>
 							<p>Design is more than a job for us; it's a passion. We see the world through the prism of graphic design, constantly searching for ways to improve ourselves and our work. By committing to this philosophy, we treat all projects as an experience. From an entire re-branding of a small business to a simple avatar for a personal blog, our mentality remains the same. Artichoke Designs exists to peel away your problems until you're left with a completely satisfying solution.</p>
 						</div>
 					</div>
 				</div>
+				<TextBanner {...text_banner_props} />
 			</div>
 		)
 	}
